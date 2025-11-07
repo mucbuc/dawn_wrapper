75ed4580f826ace16f541d6093214ac5158ff479
mark <mark@marks-MacBook-Air.local>
2025-11-06 18:28:37 -0700

    test em_build_and_run on CI

--- test/src/harness.js
+++
@@ -1,9 +0,0 @@
-const createModule = require('../web-build/Example.js');
-
-const output = [];
-createModule({
-  print: (text) => console.log(text),
-  printErr: (text) => console.error(text)
-}).then(Module => {
-  Module._main();
-});

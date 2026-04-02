4369326363f2f68a8c4beebf6b2eb25f2a77bd94
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2026-04-01 19:28:26 -0700

    rm dawn

--- lib/dawn/test/tint/bug/chromium/1367602_private_space_initializer_valid_count.wgsl.expected.dxc.hlsl
+++
@@ -1,7 +0,0 @@
-
-static const bool v_1[65535] = (bool[65535])0;
-static bool v[65535] = v_1;
-[numthreads(1, 1, 1)]
-void f() {
-}
-
--- lib/dawn/test/tint/expressions/zero_init/scalar/f16.wgsl
+++
@@ -1,6 +0,0 @@
-// flags:  --hlsl-shader-model 62
-enable f16;
-@compute @workgroup_size(1)
-fn f() {
-    var v = f16();
-}

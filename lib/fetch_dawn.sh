b6396133141f5eb019c19b8fb7df3f081b67ff5c
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2026-04-08 10:47:15 -0700

    fix up fetch_dawn with cmake

--- lib/fetch_dawn.sh
+++ lib/fetch_dawn.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 --branch chromium/7780 https://dawn.googlesource.com/dawn
+
+if [ ! -d "dawn" ]; then
+  git clone --depth 1 --branch chromium/7780 https://dawn.googlesource.com/dawn
+fi

2ab7beb974ed7905ba54949732857e989be4dd5c
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2026-04-08 10:32:02 -0700

    fetch dawn

--- lib/dawn/test/tint/builtins/bufferView/storage_unsized_runtime_array.wgsl.expected.dxc.hlsl
+++
@@ -1,5 +0,0 @@
-SKIP: FAILED
-
-buffers are not supported by the HLSL backend
-
-tint executable returned error: exit status 1
---
+++ lib/fetch_dawn.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 --branch chromium/7780 https://dawn.googlesource.com/dawn

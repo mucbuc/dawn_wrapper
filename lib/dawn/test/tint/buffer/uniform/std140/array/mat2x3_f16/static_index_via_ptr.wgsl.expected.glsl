5453d5f1077dfe18205e7700e1921f1560b7e818
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2026-04-08 09:30:00 -0700

    add dawn

---
+++ lib/dawn/test/tint/buffer/uniform/std140/array/mat2x3_f16/static_index_via_ptr.wgsl.expected.glsl
@@ -0,0 +1,49 @@
+#version 310 es
+#extension GL_AMD_gpu_shader_half_float: require
+
+layout(binding = 0, std140)
+uniform a_block_1_ubo {
+  uvec4 inner[4];
+} v;
+layout(binding = 1, std430)
+buffer s_block_1_ssbo {
+  float16_t inner;
+} v_1;
+f16vec2 tint_bitcast_to_f16_1(uint src) {
+  return unpackFloat2x16(src);
+}
+f16vec4 tint_bitcast_to_f16(uvec2 src) {
+  return f16vec4(unpackFloat2x16(src.x), unpackFloat2x16(src.y));
+}
+f16mat2x3 v_2(uint start_byte_offset) {
+  uvec4 v_3 = v.inner[(start_byte_offset / 16u)];
+  f16vec3 v_4 = tint_bitcast_to_f16(mix(v_3.xy, v_3.zw, bvec2((((start_byte_offset & 15u) >> 2u) == 2u)))).xyz;
+  uvec4 v_5 = v.inner[((8u + start_byte_offset) / 16u)];
+  return f16mat2x3(v_4, tint_bitcast_to_f16(mix(v_5.xy, v_5.zw, bvec2(((((8u + start_byte_offset) & 15u) >> 2u) == 2u)))).xyz);
+}
+f16mat2x3[4] v_6(uint start_byte_offset) {
+  f16mat2x3 a[4] = f16mat2x3[4](f16mat2x3(f16vec3(0.0hf), f16vec3(0.0hf)), f16mat2x3(f16vec3(0.0hf), f16vec3(0.0hf)), f16mat2x3(f16vec3(0.0hf), f16vec3(0.0hf)), f16mat2x3(f16vec3(0.0hf), f16vec3(0.0hf)));
+  {
+    uint v_7 = 0u;
+    v_7 = 0u;
+    while(true) {
+      uint v_8 = v_7;
+      if ((v_8 >= 4u)) {
+        break;
+      }
+      a[v_8] = v_2((start_byte_offset + (v_8 * 16u)));
+      {
+        v_7 = (v_8 + 1u);
+      }
+    }
+  }
+  return a;
+}
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+void main() {
+  f16mat2x3 l_a[4] = v_6(0u);
+  f16mat2x3 l_a_i = v_2(32u);
+  f16vec3 l_a_i_i = tint_bitcast_to_f16(v.inner[2u].zw).xyz;
+  uvec4 v_9 = v.inner[2u];
+  v_1.inner = (((tint_bitcast_to_f16_1(v_9.z).x + l_a[0u][0u].x) + l_a_i[0u].x) + l_a_i_i.x);
+}
---
+++ lib/dawn/test/tint/buffer/uniform/std140/struct/mat3x2_f32/to_private.wgsl.expected.glsl
@@ -0,0 +1,54 @@
+#version 310 es
+
+
+struct S {
+  int before;
+  mat3x2 m;
+  int after;
+};
+
+layout(binding = 0, std140)
+uniform u_block_1_ubo {
+  uvec4 inner[32];
+} v;
+S p[4] = S[4](S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0), S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0), S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0), S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0));
+mat3x2 v_1(uint start_byte_offset) {
+  uvec4 v_2 = v.inner[(start_byte_offset / 16u)];
+  vec2 v_3 = uintBitsToFloat(mix(v_2.xy, v_2.zw, bvec2((((start_byte_offset & 15u) >> 2u) == 2u))));
+  uvec4 v_4 = v.inner[((8u + start_byte_offset) / 16u)];
+  vec2 v_5 = uintBitsToFloat(mix(v_4.xy, v_4.zw, bvec2(((((8u + start_byte_offset) & 15u) >> 2u) == 2u))));
+  uvec4 v_6 = v.inner[((16u + start_byte_offset) / 16u)];
+  return mat3x2(v_3, v_5, uintBitsToFloat(mix(v_6.xy, v_6.zw, bvec2(((((16u + start_byte_offset) & 15u) >> 2u) == 2u)))));
+}
+S v_7(uint start_byte_offset) {
+  uvec4 v_8 = v.inner[(start_byte_offset / 16u)];
+  int v_9 = int(v_8[((start_byte_offset & 15u) >> 2u)]);
+  mat3x2 v_10 = v_1((8u + start_byte_offset));
+  uvec4 v_11 = v.inner[((64u + start_byte_offset) / 16u)];
+  return S(v_9, v_10, int(v_11[(((64u + start_byte_offset) & 15u) >> 2u)]));
+}
+S[4] v_12(uint start_byte_offset) {
+  S a[4] = S[4](S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0), S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0), S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0), S(0, mat3x2(vec2(0.0f), vec2(0.0f), vec2(0.0f)), 0));
+  {
+    uint v_13 = 0u;
+    v_13 = 0u;
+    while(true) {
+      uint v_14 = v_13;
+      if ((v_14 >= 4u)) {
+        break;
+      }
+      a[v_14] = v_7((start_byte_offset + (v_14 * 128u)));
+      {
+        v_13 = (v_14 + 1u);
+      }
+    }
+  }
+  return a;
+}
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+void main() {
+  p = v_12(0u);
+  p[1u] = v_7(256u);
+  p[3u].m = v_1(264u);
+  p[1u].m[0u] = uintBitsToFloat(v.inner[1u].xy).yx;
+}
